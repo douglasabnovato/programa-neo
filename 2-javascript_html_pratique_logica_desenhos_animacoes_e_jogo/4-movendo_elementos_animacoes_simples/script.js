d3c7b6b52f6adb6d6ba00921c4f88ef13de0bb14
douglasabnovato <douglasabnovato@gmail.com>
2023-05-16 09:34:18 -0300

    organizando as tarefas de hoje

--- 2-javascript_html_pratique_logica_desenhos_animacoes_e_jogo/4-movendo_elementos_animacoes_simples/script.js
+++ 2-javascript_html_pratique_logica_desenhos_animacoes_e_jogo/4-movendo_elementos_animacoes_simples/script.js
@@ -3,12 +3,7 @@ console.log("Starting ...");
 var tela = document.querySelector("canvas.animation");
 var pincel = tela.getContext("2d");
 
-pincel.fillStyle = "lightgray";
-pincel.fillRect(0, 0, 600, 400);
-
-var tela = document.querySelector("canvas");
-var pincel = tela.getContext("2d");
-pincel.fillStyle = "lightgray";
+pincel.fillStyle = "gray";
 pincel.fillRect(0, 0, 600, 400);
 
 function desenhaCirculo(x, y, raio) {
@@ -21,13 +16,49 @@ function desenhaCirculo(x, y, raio) {
 function limpaTela() {
   pincel.clearRect(0, 0, 600, 400);
 }
-
 var x = 20;
+var sentido = 1;
 
 function atualizaTela() {
   limpaTela();
+  if (x > 600) {
+    sentido = -1;
+  } else if (x < 0) {
+    sentido = 1;
+  }
   desenhaCirculo(x, 20, 10);
-  x++;
+  x = x + sentido;
 }
 
 setInterval(atualizaTela, 10);
+
+var telaA = document.querySelector("canvas.pulsar");
+var pincelA = telaA.getContext("2d");
+
+pincelA.fillStyle = "lightgray";
+pincelA.fillRect(0, 0, 600, 400);
+
+function desenhaCirculo(x, y, raio, cor) {
+  pincelA.fillStyle = cor;
+  pincelA.beginPath();
+  pincelA.arc(x, y, raio, 0, 2 * Math.PI);
+  pincelA.fill();
+}
+
+function limpaTela() {
+  pincelA.clearRect(0, 0, 600, 400);
+}
+var raio = 22;
+var fatorCrescimento = 0;
+
+function atualizaTela() {
+  limpaTela();
+  if (raio > 60) {
+    fatorCrescimento = -1;
+  } else if (raio < 23) {
+    fatorCrescimento = 1;
+  }
+  raio = raio + fatorCrescimento;
+  desenhaCirculo(300, 200, raio, "blue");
+}
+setInterval(atualizaTela, 420);
