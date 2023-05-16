fabb1db9218250920c47a6dd99aa24a441ed7ffc
douglasabnovato <douglasabnovato@gmail.com>
2023-05-16 10:53:33 -0300

    desafio final do módulo 5

--- 2-javascript_html_pratique_logica_desenhos_animacoes_e_jogo/5.-nosso_primeiro_jogo/script.js
+++ 2-javascript_html_pratique_logica_desenhos_animacoes_e_jogo/5.-nosso_primeiro_jogo/script.js
@@ -7,6 +7,8 @@ pincel.fillStyle = "lightgray";
 pincel.fillRect(0, 0, 600, 400);
 
 var raio = 10;
+var xAleatorio;
+var yAleatorio;
 
 function desenhaCirculo(x, y, raio, cor) {
   pincel.fillStyle = cor;
@@ -31,9 +33,81 @@ function sorteiaPosicao(maximo) {
 
 function atualizaTela() {
   limpaTela();
-  var xAleatorio = sorteiaPosicao(600);
-  var yAleatorio = sorteiaPosicao(400);
+  xAleatorio = sorteiaPosicao(600);
+  yAleatorio = sorteiaPosicao(400);
   desenhaAlvo(xAleatorio, yAleatorio);
 }
 
 setInterval(atualizaTela, 1000);
+
+function dispara(evento) {
+  var x = evento.pageX - tela.offsetLeft;
+  var y = evento.pageY - tela.offsetTop;
+
+  if (
+    x > xAleatorio - raio &&
+    x < xAleatorio + raio &&
+    y > yAleatorio - raio &&
+    y < yAleatorio + raio
+  ) {
+    alert("Acertou!");
+  }
+}
+
+tela.onclick = dispara;
+
+function desenhaQuadradoE(xE, yE, tamanhoE, corE) {
+  pincelE.fillStyle = corE;
+  pincelE.fillRect(xE, yE, tamanhoE, tamanhoE);
+  pincelE.fill();
+}
+
+function desenhaCirculoE(xE, yE, raioE, corE) {
+  pincelE.fillStyle = corE;
+  pincelE.beginPath();
+  pincelE.arc(xE, yE, raioE, 0, 2 * 3.14);
+  pincelE.fill();
+}
+
+function desenhaPaletaDeCoresE() {
+  desenhaQuadradoE(xVermelho, yQuadrados, tamanhoQuadrados, "red");
+  desenhaQuadradoE(xVerde, yQuadrados, tamanhoQuadrados, "green");
+  desenhaQuadradoE(xAzul, yQuadrados, tamanhoQuadrados, "blue");
+}
+
+function lidaComMovimentoDoMouseE(evento) {
+  var xE = evento.pageX - telaE.offsetLeft;
+  var yE = evento.pageY - telaE.offsetTop;
+
+  if (desenha) {
+    desenhaCirculoE(xE, yE, 5, corAtual);
+  }
+}
+
+function habilitaDesenharE() {
+  desenha = true;
+}
+
+function desabilitaDesenharE() {
+  desenha = false;
+}
+
+var telaE = document.querySelector("canvas.arte");
+var pincelE = telaE.getContext("2d");
+
+pincelE.fillStyle = "lightgray";
+pincelE.fillRect(0, 0, 600, 400);
+
+var desenha = false;
+var corAtual = "blue";
+var xVermelho = 0;
+var xVerde = 50;
+var xAzul = 100;
+var yQuadrados = 0;
+var tamanhoQuadrados = 50;
+
+desenhaPaletaDeCoresE();
+
+telaE.onmousemove = lidaComMovimentoDoMouse;
+telaE.onmousedown = habilitaDesenhar;
+telaE.onmouseup = desabilitaDesenhar;
